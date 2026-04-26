2d92e3fbeb7ad3a6441884462e4345c6566f92ce
18561265910 <18561265910@163.com>
2026-04-26 18:15:58 +0800

    el-分页

--- src/views/backend/Knowledge.vue
+++ src/views/backend/Knowledge.vue
@@ -7,15 +7,33 @@
       </template>
     </page-head>
     <table-search :form-item="formItem" @search="handleSearch" />
-    <el-table :data="tableData" stripe style="width: 100%;margin-top: 25px" highlight-current-row border
-      show-overflow-tooltip>
-      <el-table-column type="selection" label="选择文章" width="50" fixed align="center" />
-      <el-table-column type="index" label="序号" width="55" fixed align="center" />
+    <el-table
+      :data="tableData"
+      stripe
+      style="width: 100%; margin-top: 25px"
+      highlight-current-row
+      border
+      show-overflow-tooltip
+    >
+      <el-table-column
+        type="selection"
+        label="选择文章"
+        width="50"
+        fixed
+        align="center"
+      />
+      <el-table-column
+        type="index"
+        label="序号"
+        width="55"
+        fixed
+        align="center"
+      />
       <el-table-column label="文章标题" width="200" fixed align="center">
         <!-- <template #default="scope"> -->
         <!-- 这里我直接解构到底了 原来scope.row.title -->
         <template #default="{ row: { title } }">
-          <div style="display: flex;align-items: center;">
+          <div style="display: flex; align-items: center">
             <el-icon>
               <ep-timer></ep-timer>
             </el-icon>
@@ -25,7 +43,7 @@
       </el-table-column>
       <el-table-column label="分类" width="150" fixed align="center">
         <template #default="{ row: { categoryId } }">
-          <div style="display: flex;align-items: center;">
+          <div style="display: flex; align-items: center">
             <el-icon color="red">
               <ep-flag></ep-flag>
             </el-icon>
@@ -33,23 +51,61 @@
           </div>
         </template>
       </el-table-column>
-      <el-table-column prop="authorName" label="作者" width="150" align="center" />
-      <el-table-column prop="readCount" label="阅读量" width="75" align="center" />
-      <el-table-column prop="publishedAt" label="发布时间" width="200" align="center" />
-      <el-table-column prop="statusText" label="状态" width="150" align="center" />
+      <el-table-column
+        prop="authorName"
+        label="作者"
+        width="150"
+        align="center"
+      />
+      <el-table-column
+        prop="readCount"
+        label="阅读量"
+        width="75"
+        align="center"
+      />
+      <el-table-column
+        prop="publishedAt"
+        label="发布时间"
+        width="200"
+        align="center"
+      />
+      <el-table-column
+        prop="statusText"
+        label="状态"
+        width="150"
+        align="center"
+      />
 
       <el-table-column label="操作" width="200" fixed="right" align="center">
         <template #default="scope">
           <el-button-group direction="horizontal" size="small">
             <el-button type="primary" text>编辑</el-button>
-            <el-button type="success" text v-if="scope.row.status===0||scope.row.status===2">发布</el-button>
-            <el-button type="warning" text v-if="scope.row.status===1">下线</el-button>
-            <el-button type="danger" text >删除</el-button>
+            <el-button
+              type="success"
+              text
+              v-if="scope.row.status === 0 || scope.row.status === 2"
+              >发布</el-button
+            >
+            <el-button type="warning" text v-if="scope.row.status === 1"
+              >下线</el-button
+            >
+            <el-button type="danger" text>删除</el-button>
           </el-button-group>
         </template>
       </el-table-column>
     </el-table>
-
+    <el-pagination
+      style="margin-top: 20px; display: flex; justify-content: flex-end"
+      :page-sizes="[1, 5, 10, 20, 30, 50, 100]"
+      :page-size="pagination.size"
+      :total="pagination.total"
+      :pager-count="7"
+      :current-page="pagination.currentPage"
+      layout="total, sizes, prev, pager, next, jumper"
+      background
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    />
   </div>
 </template>
 
@@ -78,53 +134,70 @@ const formItem = ref([
     prop: "status",
     label: "状态",
     placeholder: "请选择状态",
-    options: [{ label: '草稿', value: '0' }, { label: '已发布', value: '1' }, { label: '已下线', value: '2' }],
+    options: [
+      { label: "草稿", value: "0" },
+      { label: "已发布", value: "1" },
+      { label: "已下线", value: "2" },
+    ],
   },
 ]);
 
-const cateMap = reactive({})// 文章分类映射
-const cateList = ref([])// 文章分类列表
+const cateMap = reactive({}); // 文章分类映射
+const cateList = ref([]); // 文章分类列表
 // 请求文章分类
 const requestCate = async () => {
-  const res = await getKnowledgeCate()
-  cateList.value = res.map(item => {
-    cateMap[item.id] = item.categoryName
+  const res = await getKnowledgeCate();
+  cateList.value = res.map((item) => {
+    cateMap[item.id] = item.categoryName;
     return {
       label: item.categoryName,
-      value: item.id
-    }
-  })
-  formItem.value[1].options = cateList.value
-}
+      value: item.id,
+    };
+  });
+  formItem.value[1].options = cateList.value;
+};
 
 //分页参数
 const pagination = reactive({
   currentPage: 1,
   size: 10,
-  total: 0
-})
+  total: 0,
+});
 // 文章列表
-const tableData = ref([])
+const tableData = ref([]);
 
 // 请求文章列表
 const requestList = async (formData) => {
   const params = {
     ...pagination,
-    ...formData
-  }
-  const { records, total: totalNum } = await getKnowledgeArticleList(params)
-  tableData.value = records
-  pagination.total = totalNum
-  
-}
+    ...formData,
+  };
+  const { records, total: totalNum } = await getKnowledgeArticleList(params);
+  tableData.value = records;
+  pagination.total = totalNum;
+  console.log(records);
+};
+// 搜索文章
 const handleSearch = (formData) => {
   // console.log(formData);
-  requestList(formData)
+  requestList(formData);
+};
+// 分页大小改变
+const handleSizeChange = (val) => {
+  console.log(`${val} items per page`);
+  pagination.size = val;
+  handleSearch();
+};
+// 分页当前页改变
+const handleCurrentChange = (page) => {
+  console.log(`current page: ${page}`);
+  pagination.currentPage = page;
+  handleSearch();
 };
 //生命周期：挂载完成后请求文章分类、文章列表
 onMounted(() => {
-  requestCate()
-  handleSearch()
+  requestCate();
+  handleSearch();
 });
 </script>
 
